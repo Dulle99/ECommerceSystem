fadf0ba34f60034a7703287ef4099785bf9fb323
Dusan <dule.sotirov99@gmail.com>
2025-02-09 17:35:48 +0100

    Directory structure changes.

---
+++ src/ProductCatalogService/ProductCatalogService/Services/IProductCatalogService.cs
@@ -0,0 +1,14 @@
+﻿using ProductCatalogService.DTOs;
+using ProductCatalogService.Models;
+
+namespace ProductCatalogService.Services
+{
+    public interface IProductCatalogService
+    {
+        public Task<Product> AddProduct(Product product);
+        public Task<Product?> GetProductById(int productId);
+        public Task<List<Product?>> GetAllProducts();
+        public Task<Product?> UpdateProduct(int productId, ProductDTO productDTO);
+        public Task<bool> DeleteProduct(int productId);
+    }
+}
